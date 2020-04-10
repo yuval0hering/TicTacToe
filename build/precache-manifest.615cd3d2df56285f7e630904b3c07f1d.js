2f99bbe27939ea7e25fe896278eb20704aea0426
yuval hering <yuvalhering@yuvals-MBP.lan>
2020-04-10 15:27:42 +0300

    tic tac toe game

---
+++ build/precache-manifest.615cd3d2df56285f7e630904b3c07f1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8482f5b680ec33ec09579a0722f7342",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfbc6433df1e85a43c08",
+    "url": "/static/css/main.8a935685.chunk.css"
+  },
+  {
+    "revision": "25c7e9c6479aa096c799",
+    "url": "/static/js/2.801887dd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.801887dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfbc6433df1e85a43c08",
+    "url": "/static/js/main.40a7b971.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
